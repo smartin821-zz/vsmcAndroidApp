0007f41483ffcb2d74d3294ed92881060c56fd20
Sean Martin <sean.martin@xamarin.com>
2016-12-08 16:21:41 -0800

    Added App

--- vsmcAndroidApp/MainActivity.cs
+++ vsmcAndroidApp/MainActivity.cs
@@ -1,6 +1,9 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using Microsoft.Azure.Mobile;
+using Microsoft.Azure.Mobile.Analytics;
+using Microsoft.Azure.Mobile.Crashes;
 
 namespace vsmcAndroidApp
 {
@@ -9,6 +12,8 @@ namespace vsmcAndroidApp
     {
         protected override void OnCreate(Bundle bundle)
         {
+            MobileCenter.Start("547b7c36-e0a6-42bf-8c16-aab779ff9a21",
+                       typeof(Analytics), typeof(Crashes));
             base.OnCreate(bundle);
 
             // Set our view from the "main" layout resource
